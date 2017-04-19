f4aef1f3d87ba283cb8d0ce52582eba8480409f5
Daeyun Shin <daeyunshin@gmail.com>
2017-04-19 13:55:00 -0500

    Fix tests.

--- tests/intersection_test.cc
+++ tests/intersection_test.cc
@@ -14,12 +14,12 @@ TEST(IntersectionTest, Sphere) {
   Vec3 origin(1, 1, -10);
   Vec3 direction(0, 0, 1);
 
-  auto ray = Ray(origin, direction);
+  auto ray = std::make_shared<Ray>(origin, direction);
 
   Intersection intersection(ray);
   sphere.IntersectRay(ray, &intersection);
 
-  auto point = ray.Point(intersection.t());
+  auto point = ray->Point(intersection.t());
 
   EXPECT_EQ(8, intersection.t());
 
@@ -37,13 +37,13 @@ TEST(IntersectionTest, Triangle) {
   Vec3 origin(0.2, 0.2, -10);
   Vec3 direction(0, 0, 1);
 
-  auto ray = Ray(origin, direction);
+  auto ray = std::make_shared<Ray>(origin, direction);
 
   Intersection intersection(ray);
   bool intersect = triangle.IntersectRay(ray, &intersection);
   EXPECT_TRUE(intersect);
 
-  auto point = ray.Point(intersection.t());
+  auto point = ray->Point(intersection.t());
 
   EXPECT_EQ(9, intersection.t());
 }
@@ -54,7 +54,7 @@ TEST(IntersectionTest, TriangleNotIntersecting) {
   Vec3 origin(0.5, 0.54, -10);
   Vec3 direction(0, 0, 1);
 
-  auto ray = Ray(origin, direction);
+  auto ray = std::make_shared<Ray>(origin, direction);
 
   Intersection intersection(ray);
   bool intersect = triangle.IntersectRay(ray, &intersection);
@@ -67,7 +67,7 @@ TEST(IntersectionTest, TransformedTriangleIntersection) {
   Vec3 origin(0.5, 0.54, -10);
   Vec3 direction(0, 0, 1);
 
-  auto ray = Ray(origin, direction);
+  auto ray = std::make_shared<Ray>(origin, direction);
 
   Intersection intersection(ray);
   bool intersect = triangle.IntersectRay(ray, &intersection);
